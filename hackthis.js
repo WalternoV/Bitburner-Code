fafb3a221f6314e2cc16d68bb05153136b9087a2
Dreicula <109347903+WalternoV@users.noreply.github.com>
2026-02-16 00:29:05 +0800

    Update hackthis.js

--- hackthis.js
+++ hackthis.js
@@ -9,7 +9,7 @@ export async function main(ns) {
   const serport = ns.getServerNumPortsRequired(server)
   
   if (relvl <= hacklvl && serport <= cport(ns)){
-    pkil(ns)
+    ns.run("portkill.js", 1, server)
     await ns.asleep(400)
     ns.run("scpfile.js", 1, server)
     await ns.asleep(400)
@@ -28,38 +28,5 @@ export async function main(ns) {
     if (ns.fileExists("SQLInject.exe")) count++;
     return count;
   }
-
-  function pkil(ns) {
-    if (ns.fileExists("SQLInject.exe")){
-    ns.sqlinject(server)
-    ns.print("SQLInject.exe executed.")
-    }
-
-    if (ns.fileExists("HTTPWorm.exe")){
-      ns.httpworm(server)
-      ns.print("HTTPWorm.exe executed.")
-    }
-
-    if (ns.fileExists("relaySMTP.exe")){
-      ns.relaysmtp(server)
-      ns.print("relaySMTP.exe executed.")
-    }
-    
-    if (ns.fileExists("FTPCrack.exe")){
-      ns.ftpcrack(server)
-      ns.print("FTPCrack.exe executed.")
-    }
-
-    if (ns.fileExists("BruteSSH.exe")){
-      ns.brutessh(server)
-      ns.print("BruteSSH.exe executed.")
-    } 
-
-    if (ns.fileExists("NUKE.exe")){
-      ns.nuke(server)
-      ns.print("NUKE.exe executed.")
-    } 
-  }
-
 
 }

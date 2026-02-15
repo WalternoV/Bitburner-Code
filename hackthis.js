a8fcfc82f3d07aaea77833389f6ae375ee9d6991
Dreicula <109347903+WalternoV@users.noreply.github.com>
2026-02-15 13:26:17 +0800

    Create hackthis.js
    Will prepared server, and get hacked.

---
+++ hackthis.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+  const server = ns.args[0]
+  const serScript = server + ".js"
+
+  ns.run("pkil.js", 1, server)
+  await ns.asleep(400)
+  ns.run("scpfile.js", 1, server)
+
+  await ns.run("srun.js", 1, server)
+}

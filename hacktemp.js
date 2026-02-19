42b58aecc0a00d37004ef1757c3661465a3a89fe
Dreicula <109347903+WalternoV@users.noreply.github.com>
2026-02-20 01:20:55 +0800

    Update hacktemp.js

--- hacktemp.js
+++ hacktemp.js
@@ -4,21 +4,21 @@ export async function main(ns) {
   let server = ns.getHostname()
   let mamon, cumon 
   let misec, selvl
+  
 
   if(server.startsWith("sub", 0,1,2)) {
+    ns.tprint("Hacking Attempt ("+  server +" in Sub Server): Success")
     server = server.slice(3)
+  } else {
+    ns.tprint("Hacking Attempt ("+  server +"): Success")
   }
-
-  ns.tprint("Hacking Attempt ("+  server +"): Success")
 
   while(true) {
     misec = ns.getServerMinSecurityLevel(server)
     selvl = ns.getServerSecurityLevel(server)
     cumon = ns.getServerMoneyAvailable(server)
     mamon = ns.getServerMaxMoney(server)
-
     
-
     if (misec < selvl) {
       await ns.weaken(server)
     } else if (cumon < mamon) {
